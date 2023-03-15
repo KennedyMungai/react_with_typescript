c3e6c289607bcf6cba6bb72ccaa21c3524e20d0e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-15 15:20:08 +0300

    Destructured data from an axios get request

--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -15,7 +15,13 @@ const searchRepositories = (term: string) =>
 
         try
         {
-
+            const { data } = await axios.get('https://registry.npmjs.org/-/v1/search',
+                {
+                    params: {
+                        text: term
+                    }
+                }
+            )
         } catch (error: any)
         {
             dispatch(
