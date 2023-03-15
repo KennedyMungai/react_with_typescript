da3b45f3a7b5769677960414a03edba7d25a96b9
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-15 14:37:58 +0300

    Added all the action types in an error

--- src/state/reducers/repositoriesReducer.tsx
+++ src/state/reducers/repositoriesReducer.tsx
@@ -25,6 +25,13 @@ interface SearchRepositoriesErrorAction
 
 type Action = SearchRepositoriesAction | SearchRepositoriesErrorAction | SearchRepositoriesSuccessAction
 
+enum ActionType
+{
+    SEARCH_REPOSITORIES = "search_repositories",
+    SEARCH_REPOSITORIES_SUCCESS = 'search_repositories_success',
+    SEARCH_REPOSITORIES_ERROR = 'search_repositories_error'
+}
+
 const reducer = (
     state: RepositoriesState,
     action: Action
