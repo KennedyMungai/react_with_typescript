356e4946fbba340fb7b6ec5002297fd739c856a7
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-15 16:23:07 +0300

    Added some simple markup to the component file

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -4,7 +4,12 @@ import React from 'react'
 const RepositoriesList = () =>
 {
     return (
-        <div>RepositoriesList</div>
+        <div>
+            <form action="">
+                <input type="text" />
+                <button>Search</button>
+            </form>
+        </div>
     )
 }
 
