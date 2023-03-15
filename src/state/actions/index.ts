e61880c9e5eca991771b67b665605f79bdd3d9b6
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-15 14:45:25 +0300

    Added the interfaces fro the various actions and the enum that holds the identifiers for the various types of actions

---
+++ src/state/actions/index.ts
@@ -0,0 +1,23 @@
+export enum ActionType
+{
+    SEARCH_REPOSITORIES = "search_repositories",
+    SEARCH_REPOSITORIES_SUCCESS = 'search_repositories_success',
+    SEARCH_REPOSITORIES_ERROR = 'search_repositories_error'
+}
+
+export interface SearchRepositoriesAction
+{
+    type: ActionType.SEARCH_REPOSITORIES
+}
+
+export interface SearchRepositoriesSuccessAction
+{
+    type: ActionType.SEARCH_REPOSITORIES_SUCCESS,
+    payload: string[]
+}
+
+export interface SearchRepositoriesErrorAction
+{
+    type: ActionType.SEARCH_REPOSITORIES_ERROR,
+    payload: string
+}
