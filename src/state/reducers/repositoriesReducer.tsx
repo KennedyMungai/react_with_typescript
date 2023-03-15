4f87c744fc181e52134969caabbb974ec88e65a7
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-15 14:11:00 +0300

    Added the return statements for the various actions

--- src/state/reducers/repositoriesReducer.tsx
+++ src/state/reducers/repositoriesReducer.tsx
@@ -13,8 +13,9 @@ const reducer = (state: RepositoriesState, action: any) =>
         case 'search_repositories':
             return { loading: true, error: null, data: [] }
         case 'search_repositories_success':
+            return { loading: false, error: null, data: action.payload }
         case 'search_repositories_error':
-
+            return { loading: false, error: action.payload, data: [] }
         default:
             return state
     }
