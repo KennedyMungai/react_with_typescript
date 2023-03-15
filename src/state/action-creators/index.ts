e301a8c25b49b9ef67d10084ac8bf84f20ccdff7
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-15 15:28:28 +0300

    Wrote the logic to search for ans retrieve package names from the npm js api endpoint

--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -22,6 +22,16 @@ const searchRepositories = (term: string) =>
                     }
                 }
             )
+
+            const names = data.objects.map((result: any) => 
+            {
+                return result.package.name
+            })
+
+            dispatch({
+                type: ActionType.SEARCH_REPOSITORIES_SUCCESS,
+                payload: names
+            })
         } catch (error: any)
         {
             dispatch(
