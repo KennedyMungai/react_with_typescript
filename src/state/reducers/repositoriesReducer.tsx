5141990f3023e8d68fc92f0cd566b032ad4368ab
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-15 14:45:46 +0300

    Moved th einterfaces

--- src/state/reducers/repositoriesReducer.tsx
+++ src/state/reducers/repositoriesReducer.tsx
@@ -6,31 +6,9 @@ interface RepositoriesState
     data: string[]
 }
 
-interface SearchRepositoriesAction
-{
-    type: ActionType.SEARCH_REPOSITORIES
-}
-
-interface SearchRepositoriesSuccessAction
-{
-    type: ActionType.SEARCH_REPOSITORIES_SUCCESS,
-    payload: string[]
-}
-
-interface SearchRepositoriesErrorAction
-{
-    type: ActionType.SEARCH_REPOSITORIES_ERROR,
-    payload: string
-}
 
 type Action = SearchRepositoriesAction | SearchRepositoriesErrorAction | SearchRepositoriesSuccessAction
 
-enum ActionType
-{
-    SEARCH_REPOSITORIES = "search_repositories",
-    SEARCH_REPOSITORIES_SUCCESS = 'search_repositories_success',
-    SEARCH_REPOSITORIES_ERROR = 'search_repositories_error'
-}
 
 const reducer = (
     state: RepositoriesState,
