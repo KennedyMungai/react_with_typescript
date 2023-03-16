ff02cea8416ad077f73d410da21a596073dd911a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-16 09:18:23 +0300

    Added some conditionally rendered markup

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -32,6 +32,10 @@ const RepositoriesList = () =>
                 />
                 <button>Search</button>
             </form>
+
+            {error && <h3>{error}</h3>}
+            {loading && <h3>Loading...</h3>}
+            {!error && !loading && data}
         </div>
     )
 }
